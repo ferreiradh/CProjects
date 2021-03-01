0cb3935197f4360e3bbbff75732d1906face727c
ferreiradh <79875791+ferreiradh@users.noreply.github.com>
2021-03-01 18:56:49 -0300

    Create Verificador_triangulo.cpp

---
+++ Triangulo/Verificador_triangulo.cpp
@@ -0,0 +1,29 @@
+
+// Este é um programa para verificar se as medidas inseridas são de um triângulo ou não e para classificá-lo como equilátero, isósceles ou escaleno
+// Criado por Henrique Ferreira
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+#include <stdlib.h>
+
+main()
+{
+	setlocale (LC_ALL, "Portuguese");
+    float l1, l2, l3;
+
+    printf("Digite o valor de cada lado:");
+    scanf("%f %f %f",&l1,&l2,&l3); 
+    if (((l2-l3<l1)&&(l1<l2+l3))&&((l3-l1<l2)&&(l2<l3+l1))&&((l1-l2<l3)&&(l3<l2+l1))){
+    printf ("Estes valores formam um triângulo\n");
+    if (l1==l2 && l2==l3)
+    printf ("Este é um triângulo Equilátero\n");
+    else if (l1==l2 || l1==l3 || l2==l3)
+    printf ("Este é um triângulo Isósceles\n");
+    else if (l1 != l2 && l1 != l3 && l3 != l2)
+    printf ("Este é um triângulo Escaleno\n");
+
+}
+    else
+      printf ("Estes valores não formam um triângulo\n");
+}
